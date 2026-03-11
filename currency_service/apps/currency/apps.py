1bea3d0b8b425e61713739867412d7b4406664a2
mykhailo.pravdiuk <mykhailo.pravdiuk@semiotics-lab.com>
2026-03-11 14:38:04 +0200

    Add sync rates call in ready method

--- currency_service/apps/currency/apps.py
+++ currency_service/apps/currency/apps.py
@@ -1,5 +1,15 @@
 from django.apps import AppConfig
+import sys
 
 
 class CurrencyConfig(AppConfig):
     name = "apps.currency"
+    
+    def ready(self):
+        if 'runserver' in sys.argv:
+            from apps.currency.services import ExchangeRateService
+            try:
+                print("Filling db with initial data...")
+                result = ExchangeRateService.sync_rates()
+            except Exception as e:
+                print(f"Could not perform startup sync: {e}")
